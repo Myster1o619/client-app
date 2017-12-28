877f0b56590fd9aa8f0b0a6306a002aab411a817
Andrew Green <myster1o619@hotmail.com>
2017-12-28 07:04:23 +0200

    user registration & validation for login

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessagesService: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.authService.register(this.email, this.password).
+    then(response => {
+      this.router.navigate(['/login']);
+      this.flashMessagesService.show('You have successfully registered. Please log in with your new account details.', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      // this.router.navigate(['/']);
+    }).catch (error => {
+      this.flashMessagesService.show(error.message, {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    });
   }
 
 }
